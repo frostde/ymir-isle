18c3e27b22d7bf75c5fd6802ce4de6b90b1184c0
danielfrost <frostde93@gmail.com>
2021-03-18 23:21:06 -0400

    ci: updating main yaml

--- src/pages/about.test.ts
+++ src/pages/about.test.ts
@@ -3,7 +3,5 @@ import * as React from "react";
 
 describe("src -> pages -> about", () => {
     test("should display a blank login form, with remember me checked by default", async () => {
-      // ???
-
     });
 });
