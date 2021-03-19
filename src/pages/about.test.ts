9cff3c8a07a6824ef02a9f7f24c6c8e943394123
danielfrost <frostde93@gmail.com>
2021-03-18 23:17:19 -0400

    test: adding initial test

---
+++ src/pages/about.test.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+import * as React from "react";
+
+describe("src -> pages -> about", () => {
+    test("should display a blank login form, with remember me checked by default", async () => {
+      // ???
+
+    });
+});
