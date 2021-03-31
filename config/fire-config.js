c240c1f1c56cb18107b98b27e642c50707181c82
danielfrost <frostde93@gmail.com>
2021-03-30 23:42:06 -0400

    chore: allowing console log in config file

--- config/fire-config.js
+++ config/fire-config.js
@@ -12,6 +12,7 @@ try {
     firebase.initializeApp(firebaseConfig)
 } catch (err) {
     if (!/already exists/.test(err.message)) {
+        // eslint-disable-next-line no-console
         console.error('Firebase initialization error', err.stack)
     }
 }
